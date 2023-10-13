8a01cff89edfc8a98a2b88ca29ee95356e6ee5d5
Pragya352 <96974655+Pragya352@users.noreply.github.com>
2023-10-13 13:22:16 +0530

    User updated.

--- src/main/java/com/example/frontdoor/controller/ProductController.java
+++ src/main/java/com/example/frontdoor/controller/ProductController.java
@@ -24,7 +24,7 @@ public class ProductController {
     
     @GetMapping("/user")
     public String user(){
-        return "Hello User1!!!";
+        return "Hello User1!!";
     }
 
     @PostMapping
